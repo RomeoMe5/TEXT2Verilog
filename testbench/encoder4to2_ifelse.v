363e0db2fbe04847019dfe635790aef2f3c24458
Daniil Yaschenko <yaschenkodanya25@gmail.com>
2024-05-17 15:49:20 +0300

    rm testbech

--- testbench/encoder4to2_ifelse.v
+++
@@ -1,1 +0,0 @@
-module encoder4to2_ifelse (input [3:0] in, output reg [1:0] out);     always @(*) begin   if (in[0]) out = 2'b00;   else if (in[1]) out = 2'b01;   else if (in[2]) out = 2'b10;   else if (in[3]) out = 2'b11;   else out = 2'bxx;   end     endmodule
--- testbench/full_subtractor.v
+++
@@ -1,4 +0,0 @@
-module full_subtractor (input a, input b, input bin, output difference, output bout); 
-assign difference = a ^ b ^ bin; 
-assign bout = (!a & b) | (!a & bin) | (b & bin); 
-endmodule
